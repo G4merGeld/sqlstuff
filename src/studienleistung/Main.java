404c15e75c07ed86e971a983706e07b764c6cc00
G4merGeld <67011875+G4merGeld@users.noreply.github.com>
2023-11-30 03:30:58 +0100

    restructured database filled database with data implemented queryall implemented 4 queries

--- src/studienleistung/Main.java
+++ src/studienleistung/Main.java
@@ -42,25 +42,32 @@ public class Main {
         return String.join(System.lineSeparator(), RL);
     }
 
+
+    //Create Tables
     static void StructDB() {
         try {
             Statement stmt = connex.createStatement();
             int result;
-            // Tabelle Genre: [GenreID | Genre | MarketShare]
-            result = stmt.executeUpdate("DROP TABLE IF EXISTS Genre CASCADE ");
-            result = stmt.executeUpdate(("CREATE TABLE Genre(GenreID integer UNIQUE NOT NULL PRIMARY KEY, Genre text NOT NULL, MarketShare integer NOT NULL)"));
+            //Tabelle Game: [Game | ReleaseDate | PeakViewerShip]
+            result = stmt.executeUpdate("DROP TABLE IF EXISTS Game CASCADE");
+            result = stmt.executeUpdate("CREATE TABLE Game(Game text UNIQUE NOT NULL PRIMARY KEY, ReleaseDate date NOT NULL, PeakViewership integer NOT NULL)");
 
-            // Tabelle GameInfo : [Game | Developer | ReleaseYear | SoldCopies]
-            result = stmt.executeUpdate("DROP TABLE IF EXISTS GameInfo CASCADE ");
-            result = stmt.executeUpdate("CREATE TABLE GameInfo(Game text UNIQUE NOT NULL PRIMARY KEY , Developer text NOT NULL, ReleaseYear integer NOT NULL, SoldCopies integer NOT NULL)");
+            //Tabelle Player: [PlayerID | PlayerForname | GamerTag | PlayerSurname| Game]
+            result = stmt.executeUpdate("DROP TABLE IF EXISTS Player CASCADE");
+            result = stmt.executeUpdate("CREATE TABLE Player(PlayerID integer UNIQUE NOT NULL PRIMARY KEY, PlayerForname text NOT NULL, GamerTag text NOT NULL, PlayerSurname text NOT NULL, Game text NOT NULL REFERENCES Game)");
 
+            //Tabelle Owner: [OwnerID | OwnerSurname | OwnerForname]
+            result = stmt.executeUpdate("DROP TABLE IF EXISTS Owner CASCADE");
+            result = stmt.executeUpdate("CREATE TABLE Owner(OwnerID integer UNIQUE NOT NULL PRIMARY KEY, OwnerSurname text NOT NULL, OwnerForname text NOT NULL)");
 
-            // Tabelle Game: [GenreID | Game]
+            //Tabelle Team: [Team | OwnerID | Founded]
+            result = stmt.executeUpdate("DROP TABLE IF EXISTS Team CASCADE");
+            result = stmt.executeUpdate("CREATE TABLE Team(Team text unique NOT NULL PRIMARY KEY, OwnerID integer UNIQUE NOT NULL REFERENCES Owner, Founded date NOT NULL)");
 
-            result = stmt.executeUpdate("DROP TABLE IF EXISTS Game CASCADE ");
-            result = stmt.executeUpdate("CREATE TABLE Game(GenreID integer NOT NULL REFERENCES Genre, Game text NOT NULL REFERENCES GameInfo)");
+            //Tabelle PlaysIn: [PlayerID | Team]
+            result = stmt.executeUpdate("DROP TABLE IF EXISTS PlaysIn CASCADE ");
+            result = stmt.executeUpdate("CREATE TABLE PlaysIn(PlayerID integer NOT NULL REFERENCES Player, Team text NOT NULL REFERENCES Team)");
             stmt.close();
-
         } catch (SQLException E) {
             System.out.println("Exception: " + E.getMessage());
         }
@@ -72,12 +79,52 @@ public class Main {
             Statement stmt = connex.createStatement();
             int result;
 
-            // Tabelle Genre
-            result = stmt.executeUpdate("DELETE FROM Genre"); //wipe tabelle clean
+            //Game
+            result = stmt.executeUpdate("DELETE FROM Game");
+            stmt.addBatch("INSERT INTO Game VALUES('League of Legends', '2009-10-27', 6402760)");
+            stmt.addBatch("INSERT INTO Game VALUES('Dota 2', '2013-07-09', 2741514)");
+            stmt.addBatch("INSERT INTO Game VALUES('Counter-Strike: Global Offensive','2012-08-21', 2748434)");
+            stmt.addBatch("INSERT INTO Game VALUES('Fortnite', '2017-07-21', 2334826)");
+            stmt.addBatch("INSERT INTO Game VALUES('Valorant', '2020-06-02', 1505804)");
+            stmt.executeBatch();
 
-            result = stmt.executeUpdate("INSERT INTO Genre VALUES (0, 'Shooter', 66)");
-            result = stmt.executeUpdate("INSERT INTO Genre VALUES (1, 'Action adventure', 62)");
-            result = stmt.executeUpdate("INSERT INTO Genre VALUES (2, 'Simulation', 43)");
+            //Player
+            result = stmt.executeUpdate("DELETE FROM Player");
+            stmt.addBatch("INSERT INTO Player VALUES(0, 'Lee', 'Faker', 'Sang-hyeok', 'League of Legends')");
+            stmt.addBatch("INSERT INTO Player VALUES(1, 'Oleksandr', 's1mple', 'Kostyljev', 'Counter-Strike: Global Offensive')");
+            stmt.addBatch("INSERT INTO Player VALUES(2, 'Danil', 'Dendi', 'Ishutin', 'Dota 2')");
+            stmt.addBatch("INSERT INTO Player VALUES(3, 'Olof', 'olofmeister', 'Kajbjer', 'Counter-Strike: Global Offensive')");
+            stmt.addBatch("INSERT INTO Player Values(4, 'Martin', 'Rekkles', 'Larsson', 'League of Legends')");
+            stmt.executeBatch();
+
+            //Owner
+            result = stmt.executeUpdate("DELETE FROM Owner");
+            stmt.addBatch("INSERT INTO Owner VALUES(0, 'Matthews', 'Sam')");
+            stmt.addBatch("INSERT INTO Owner VALUES(1, 'Marsh', 'Joe')");
+            stmt.addBatch("INSERT INTO Owner VALUES(2, 'Zolotarov', 'Yevhen')");
+            stmt.addBatch("INSERT INTO Owner VALUES(3, 'Etienne', 'Jack')");
+            stmt.addBatch("INSERT INTO Owner VALUES(4, 'Dinh', 'Andy')");
+            stmt.addBatch("INSERT INTO Owner VALUES(5, 'Bengtson', 'Richard')");
+            stmt.executeBatch();
+
+            //Team
+            result = stmt.executeUpdate("DELETE FROM Team");
+            stmt.addBatch("INSERT INTO Team VALUES('FaZe Clan', 5, '2010-05-30')");
+            stmt.addBatch("INSERT INTO Team VALUES('T1', 1, '2014-04-13')");
+            stmt.addBatch("INSERT INTO TEAM VALUES('Cloud9', 3, '2013-04-01')");
+            stmt.addBatch("INSERT INTO Team VALUES('Fnatic', 0, '2004-07-23')");
+            stmt.addBatch("INSERT INTO Team VALUES('Natus Vincere', 2, '2009-12-17')");
+            stmt.addBatch("INSERT INTO TEAM VALUES('Team SoloMid', 4, '2011-01-01')");
+            stmt.executeBatch();
+
+            //PlaysIn
+            result = stmt.executeUpdate("DELETE FROM PlaysIn");
+            stmt.addBatch("INSERT INTO PlaysIn VALUES(0, 'T1')");
+            stmt.addBatch("INSERT INTO PlaysIn VALUES(1, 'Natus Vincere')");
+            stmt.addBatch("INSERT INTO PlaysIn VALUES(2, 'Natus Vincere')");
+            stmt.addBatch("INSERT INTO PlaysIn VALUES(3, 'Fnatic')");
+            stmt.addBatch("INSERT INTO PlaysIn VALUES(4, 'Fnatic')");
+            stmt.executeBatch();
 
             stmt.close();
         } catch(SQLException E) {
@@ -90,31 +137,115 @@ public class Main {
             Statement stmt = connex.createStatement();
             ResultSet resultSet;
 
-            resultSet = stmt.executeQuery("SELECT * FROM Genre");
+            System.out.println("GAMES");
+            resultSet = stmt.executeQuery("SELECT * FROM Game");
+            System.out.println(ResultToString(resultSet));
+
+            System.out.println("PLAYERS");
+            resultSet = stmt.executeQuery("SELECT * FROM Player");
             System.out.println(ResultToString(resultSet));
             resultSet.close();
 
+            System.out.println("OWNERS");
+            resultSet = stmt.executeQuery("SELECT * FROM Owner");
+            System.out.println(ResultToString(resultSet));
+            resultSet.close();
+
+            System.out.println("TEAMS");
+            resultSet = stmt.executeQuery("SELECT * FROM Team");
+            System.out.println(ResultToString(resultSet));
+            resultSet.close();
+
+            System.out.println("PlaysIn");
+            resultSet = stmt.executeQuery("SELECT * FROM PlaysIn");
+            System.out.println(ResultToString(resultSet));
+            resultSet.close();
 
             stmt.close();
         } catch(SQLException E) {
             System.out.println("QueryAll error: " + E.getMessage());
         }
     }
+
+    //i)
+    static void QueryDB_1() {
+        try {
+            Statement stmt = connex.createStatement();
+            ResultSet resultSet;
+            resultSet = stmt.executeQuery("SELECT GamerTag FROM Player");
+            System.out.println("All gamertags");
+            System.out.println(ResultToString(resultSet));
+            resultSet.close();
+
+            stmt.close();
+        } catch (SQLException E) {
+            System.out.println("Query 1 error: " + E.getMessage());
+        }
+    }
+
+    static void QueryDB_2() {
+        try {
+            Statement stmt = connex.createStatement();
+            ResultSet resultSet;
+            resultSet = stmt.executeQuery("SELECT Team FROM Team WHERE Founded BETWEEN '20000101' AND '20101230'");
+            System.out.println("All teams formed between 2001 and 2011");
+            System.out.println(ResultToString(resultSet));
+            resultSet.close();
+
+            stmt.close();
+        } catch (SQLException E) {
+            System.out.println("Query 2 error: " + E.getMessage());
+        }
+    }
+
+    static void QueryDB_3() {
+        try {
+            Statement stmt = connex.createStatement();
+            ResultSet resultSet;
+            resultSet = stmt.executeQuery("SELECT OwnerSurname, OwnerForname FROM Owner WHERE OwnerSurname LIKE 'M%' OR OwnerForname LIKE 'J%'");
+            System.out.println("All team owners whose surname start with m or forname start with j");
+            System.out.println(ResultToString(resultSet));
+            resultSet.close();
+
+            stmt.close();
+        } catch (SQLException E) {
+            System.out.println("Query 3 error: " + E.getMessage());
+        }
+    }
+
+    static void QueryDB_4() {
+        try {
+            Statement stmt = connex.createStatement();
+            ResultSet resultSet;
+            resultSet = stmt.executeQuery("SELECT Game, PeakViewership FROM Game ORDER BY PeakViewership DESC");
+            System.out.println("All games descendengly ordered by peak viewership");
+            System.out.println(ResultToString(resultSet));
+            resultSet.close();
+
+            stmt.close();
+        } catch (SQLException E) {
+            System.out.println("Query 4 error: " + E.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
         try {
             Class.forName("org.postgresql.Driver");
         } catch(ClassNotFoundException E) {
-            System.out.println("Exception : " + E.getMessage());
+            System.out.println(" Driver Exception : " + E.getMessage());
         }
 
         try {
-            connex = DriverManager.getConnection("jdbc:postgresql:GameGenres", "postgres", "blubb");
+            connex = DriverManager.getConnection("jdbc:postgresql:ESport", "postgres", "blubb");
 
 
             StructDB();
             UpdateDB();
-            QueryAll();
-
+            //QueryAll();
+            QueryDB_1();
+            QueryDB_2();
+            QueryDB_3();
+            QueryDB_4();
 
             connex.close();
         } catch(SQLException E) {
